b31297ca7cbe3202744c75fd5b13818d7aa3b692
phantom <GOA>
2014-05-29 02:03:01 +0300

    change lib location ( bug in  8.7 also)

--- build/kits/embedded/examples/embedded-portfolio/run.bat
+++ build/kits/embedded/examples/embedded-portfolio/run.bat
@@ -1,4 +1,4 @@
-TEIID_PATH=..\..\lib\*;..\..\optional\translator-file-${pom.version}.jar;..\..\optional\connector-file-${pom.version}-lib.jar;..\..\optional\translator-jdbc-${pom.version}.jar;h2-1.3.161.jar
+SET TEIID_PATH=..\..\lib\*;..\..\optional\file\translator-file-${pom.version}.jar;..\..\optional\file\connector-file-${pom.version}-lib.jar;..\..\optional\jbdc\translator-jdbc-${pom.version}.jar;h2-1.3.161.jar
 
 javac -cp %TEIID_PATH% src\org\teiid\example\*.java 
 
